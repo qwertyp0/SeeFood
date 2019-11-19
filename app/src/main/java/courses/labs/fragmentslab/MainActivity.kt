cbb5644e048e49058921bc2ebb5b12d7c9f8181e
Logan  Liao <loganliao@wireless-10-105-166-27.umd.edu>
2019-11-19 12:02:37 -0500

    pushing build

--- app/src/main/java/courses/labs/fragmentslab/MainActivity.kt
+++ app/src/main/java/courses/labs/fragmentslab/MainActivity.kt
@@ -3,12 +3,16 @@ package course.labs.fragmentslab
 import course.labs.fragmentslab.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import androidx.appcompat.widget.Toolbar
+// import android.support.v7.widget.Toolbar
 import androidx.fragment.app.FragmentManager
 
 
 
 private var mFragmentManager: FragmentManager? = null
 private val mLoadingFragment = LoadingFragment()
+private val mAccountFragment = AccountFragment()
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,11 +20,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setToolbar()
+
         mFragmentManager = supportFragmentManager
 
         val mFragmentTransaction = mFragmentManager!!.beginTransaction()
         mFragmentTransaction.add(R.id.fragment_container, mLoadingFragment)
         mFragmentTransaction.commit()
         mFragmentManager!!.executePendingTransactions()
+    }
+
+    private fun setToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionbar = getSupportActionBar()
+        actionbar!!.setDisplayHomeAsUpEnabled(true)
+        actionbar!!.setHomeAsUpIndicator(R.drawable.ic_home)
     }
 }
