773604d41e476ffd4906c1d117260857c12ee7ad
Logan  Liao <loganliao@wireless-10-105-166-27.umd.edu>
2019-11-18 22:55:38 -0500

    Added in loading fragment and activity should start with that right away

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+
+
+private var mFragmentManager: FragmentManager? = null
+private val mLoadingFragment = LoadingFragment()
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFragmentManager = supportFragmentManager
+
+        val mFragmentTransaction = mFragmentManager!!.beginTransaction()
+        mFragmentTransaction.add(R.id.fragment_container, mLoadingFragment)
+        mFragmentTransaction.commit()
+        mFragmentManager!!.executePendingTransactions()
     }
 }
