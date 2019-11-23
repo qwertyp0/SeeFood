6f20d4eb5255103796bb81656eef064f7b026a91
Hackerrank <abibcortes@hackerrank.com>
2019-11-22 22:50:58 -0500

    Added oAuth and database connection

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -0,0 +1,98 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class LoginActivity : AppCompatActivity() {
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var userEmail: EditText? = null
+    private var userPassword: EditText? = null
+    private var loginBtn: Button? = null
+    private var progressBar: ProgressBar? = null
+
+    private var mAuth: FirebaseAuth? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference!!.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+
+        initializeUI()
+
+        loginBtn!!.setOnClickListener { loginUserAccount() }
+    }
+
+    private fun loginUserAccount() {
+        progressBar!!.visibility = View.VISIBLE
+        val email: String
+        val password: String
+        email = userEmail!!.text.toString()
+        password = userPassword!!.text.toString()
+
+        // Todo : Retrieve email and password, make sure it's not empty
+
+
+        if (email.equals("") || password.equals("")) {
+            Toast.makeText(applicationContext, "Login failed! Please make sure email and password are filled in", Toast.LENGTH_LONG).show()
+        }
+        else {
+
+            // Todo : Sign in with given Email and Password
+            mAuth!!.signInWithEmailAndPassword(email,password).
+                addOnCompleteListener{task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this@LoginActivity,MainActivity::class.java)
+                        intent.putExtra(UserID,mAuth!!.getCurrentUser()?.uid)
+                        Log.i(TAG,"The current user id is : " + mAuth!!.getCurrentUser()?.uid)
+                        startActivity(intent)
+                    }
+                    else {
+                        Toast.makeText(applicationContext,"Email or password is incorrect. Please try again",Toast.LENGTH_LONG)
+                    }
+                }
+            // Retrieve UID for Current User if Login successful and store in intent, for the key UserID
+            // Start Intent DashboardActivity if Registration Successful
+
+
+        }
+
+
+
+    }
+
+    private fun initializeUI() {
+        userEmail = findViewById(R.id.email)
+        userPassword = findViewById(R.id.password)
+
+        loginBtn = findViewById(R.id.login)
+        progressBar = findViewById(R.id.progressBar)
+    }
+
+    companion object {
+        val TAG = "LAB 6"
+        val UserMail = "com.example.tesla.myhomelibrary.UMail"
+        val UserID = "com.example.tesla.myhomelibrary.UID"
+
+    }
+}
+
---
+++ app/src/main/java/com/example/myapplication/MainEntryActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainEntryActivity : AppCompatActivity() {
+
+    internal var registerBtn: Button? = null
+    internal var loginBtn: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_entry)
+
+        initializeViews()
+
+        registerBtn!!.setOnClickListener {
+            val intent = Intent(this@MainEntryActivity, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+        loginBtn!!.setOnClickListener {
+            val intent = Intent(this@MainEntryActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun initializeViews() {
+        registerBtn = findViewById(R.id.register)
+        loginBtn = findViewById(R.id.login)
+    }
+}
+
