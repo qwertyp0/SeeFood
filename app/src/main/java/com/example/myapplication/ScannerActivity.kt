bbdb7ab053efa6b5d0bd587be792bc78d341f239
Yan <yan_w@live.com>
2019-11-23 02:34:53 -0500

    scanner dummy file

---
+++ app/src/main/java/com/example/myapplication/ScannerActivity.kt
@@ -0,0 +1,22 @@
+
+package com.example.myapplication
+
+/*
+Still in progress
+NutritionLabel object is returned through putExtra when activity finishes
+Feel free to change the object
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class NutritionLabel(val product : String = "product",
+                     val servings : Double = 0.0,
+                     val calories : Int = 0,
+                     val totalFat : Int = 0,
+                     val cholesterol : Int = 0,
+                     val sodium : Int = 0,
+                     val totalCarbohydrate : Int = 0,
+                     val protein : Int = 0) : Parcelable
+                     
+*/
