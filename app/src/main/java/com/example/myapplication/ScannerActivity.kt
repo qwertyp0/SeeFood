342b10808ad053423dc14f433e61ebffa8cfc16d
Logan  Liao <loganliao@Logans-MacBook-Pro.local>
2019-11-26 16:34:19 -0500

    added bar graph to home fragment

--- app/src/main/java/com/example/myapplication/ScannerActivity.kt
+++ app/src/main/java/com/example/myapplication/ScannerActivity.kt
@@ -18,5 +18,32 @@ data class NutritionLabel(val product : String = "product",
                      val sodium : Int = 0,
                      val totalCarbohydrate : Int = 0,
                      val protein : Int = 0) : Parcelable
-                     
+
+// TODO Yan is this possbile to get?
+Total Fat
+Saturated Fat
+Trans Fat
+Cholesterol
+Sodium
+Potassium
+Total Carbohydrates
+Fiber
+Sugar
+protein
+Vitamin A
+Vitamin B6
+Vitamin B12
+Vitamin C
+Vitamin D
+Calcium
+Iron
+Thiamin
+Riboflavin
+Niacin
+Folate
+Biotin
+Pantothenic Acid
+Vitamin K
+Vitamin E
+
 */
