524309fc82980489d0eedfcb007b0cfcd4d2c588
Logan Liao <logan.liao97@gmail.com>
2019-12-03 22:46:26 -0500

    getting rid of all changes in home frag

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -111,33 +111,32 @@ class HomeFragment : Fragment() {
         mFragmentManager = fragmentManager
 
 
-        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
-            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
-                                   dayOfMonth: Int) {
-                cal?.set(Calendar.YEAR, year)
-                cal?.set(Calendar.MONTH, monthOfYear)
-                cal?.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-                mFragmentManager!!.popBackStackImmediate()
-                updateDateInView()
-                fillCharts(userId.toString(),mDateView!!.text.toString())
-                makeBarGraph()
-
-                //fillCharts(userId.toString(),mDateView!!.text.toString())
-            }
-        }
-        mDateView!!.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(view: View) {
-                DatePickerDialog(context!!,
-                    dateSetListener,
-                    // set DatePickerDialog to point to today's date when it loads up
-                    cal!!.get(Calendar.YEAR),
-                    cal!!.get(Calendar.MONTH),
-                    cal!!.get(Calendar.DAY_OF_MONTH)).show()
-                    updateDateInView()
-
-            }
-
-        })
+//        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+//            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
+//                                   dayOfMonth: Int) {
+//                cal?.set(Calendar.YEAR, year)
+//                cal?.set(Calendar.MONTH, monthOfYear)
+//                cal?.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+//                mFragmentManager!!.popBackStackImmediate()
+//                updateDateInView()
+//                fillCharts(userId.toString(),mDateView!!.text.toString())
+//                makeBarGraph()
+//
+//                //fillCharts(userId.toString(),mDateView!!.text.toString())
+//            }
+//        }
+//        mDateView!!.setOnClickListener(object : View.OnClickListener {
+//            override fun onClick(view: View) {
+//                DatePickerDialog(context!!,
+//                    dateSetListener,
+//                    // set DatePickerDialog to point to today's date when it loads up
+//                    cal!!.get(Calendar.YEAR),
+//                    cal!!.get(Calendar.MONTH),
+//                    cal!!.get(Calendar.DAY_OF_MONTH)).show()
+//                    updateDateInView()
+//            }
+//
+//        })
 
         mFloatingActionButton = view.findViewById(R.id.floating_action_button)
         mFloatingActionButton!!.setOnClickListener {
